86050e6b7c7dc50485974417691cee453bb60bf6
adamtb25 <142275618+adamtb25@users.noreply.github.com>
2024-01-20 05:06:32 +0000

    long time

--- cam-v1/Runner.java
+++ cam-v1/Runner.java
@@ -30,8 +30,8 @@ public class Runner {
     }
 
     public static void main(String[] args) {
-        VirtualPet p1 = new VirtualPet();
-        p1.feed();
+        //VirtualPet p1 = new VirtualPet();
+        
         
     }
 }
